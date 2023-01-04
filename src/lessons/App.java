364ae2e985a702928029b87a25a3cad3974603e9
annasavasteeva <anna.savasteeva@gmail.com>
2023-01-04 17:28:27 +0300

    Add Lib with docs

---
+++ src/lessons/App.java
@@ -0,0 +1,10 @@
+package lessons;
+
+import static lessons.Lib.printHello;
+
+public class App {
+
+    public static void main(String[] args) {
+        printHello("Вася");
+    }
+}
---
+++ src/lessons/Lib.java
@@ -0,0 +1,14 @@
+package lessons;
+
+public class Lib {
+
+    /**
+     * Выводит в консоль приветствие
+     *
+     * @param name имя того, кого нужно поприветствовать
+     */
+
+    public static void printHello(String name) {
+        System.out.printf("Hello, %s!%n", name);
+    }
+}
